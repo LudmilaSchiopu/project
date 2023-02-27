8286d7567f4cbd5d12834a8d7cf10495909833db
Liuda <liuda.sci@gmail.com>
2023-02-27 13:00:49 +0200

    common task nr.4

--- js/script.js
+++ js/script.js
@@ -743,7 +743,7 @@ false - выводит в консоль главный объект прогр�
 genres
 P.S. Функции вызывать не обязательно */
 
-let numberOfFilms3;
+/*let numberOfFilms3;
 
 function start () {
     numberOfFilms3 = +prompt('Сколько фильмов вы уже посмотрели?' , '');
@@ -813,7 +813,7 @@ function writeYourGenres () {
         personalMovieDB3.genres[i - 1] = genre;
     }
 }
-writeYourGenres ();
+writeYourGenres ();*/
 
 
 //zadacia 7
@@ -1105,5 +1105,105 @@ function availableCurr(arr, missingCurr) {
     }
 
 console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'));
+
+
+//prototipi
+const soldier = {
+    health: 400,
+    power: 500,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+const john = Object.create(soldier);
+//Object.setPrototypeOf(soldier);
+john.sayHello();
+
+/* Задание на урок:
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+
+
+const personalMovieDB4 = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        personalMovieDB4.count = +prompt('Сколько фильмов вы уже посмотрели?' , '');
+    
+        while (personalMovieDB4.count == '' || personalMovieDB4.count == null || isNaN (personalMovieDB4.count)) {
+            personalMovieDB4.count = +prompt('Сколько фильмов вы уже посмотрели?' , '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt ('Один из последних просмотренных фильмов?', '').trim(),
+                  b = prompt ('На сколько оцените его?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB4.movies[a] = b;  
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB4.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB4.count >= 10 && personalMovieDB4.count < 30) {
+            console.log("Вы классический зритель");
+        } else if ( personalMovieDB4.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB4);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB4.privat) {
+            personalMovieDB4.privat = false;
+        } else {
+            personalMovieDB4.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt (`Ваш любимый жанр под номером ${i}`);
+
+            if (genre === '' || genre === 'null') {
+                console.log('Vi ne vveli daniie');
+                i--;
+            } else {
+                personalMovieDB4.genres[i - 1] = genre;
+            }
+            
+        }
+        personalMovieDB4.genres.forEach((item, i) => {
+            console.log(`Любимый жанр #${i + 1} - это ${item}`);
+        });
+    }
+};
+
+
+
+
+
 
 
